00314a1c8e30d63b8c5d4690e9eb8e6f2c83fd13
Mohammad Hamdallah <hamdallahmohammad3219@gmail.com>
2025-02-07 01:11:15 -0600

    Test

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},  // Add autoprefixer plugin
-  },
-};
-
-export default config;
