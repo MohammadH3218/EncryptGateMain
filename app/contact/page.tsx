ee51d7331746e5a4edd99359896859786c547e09
Mohammad Hamdallah <hamdallahmohammad3219@gmail.com>
2025-02-07 04:12:30 -0600

    possible fix for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,7 +35,7 @@ function ContactForm() {
     }
 
     try {
-      const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000"
+      const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || "https://encryptgate.net"
 
       // Make a POST request to your backend /send-email route
       const response = await fetch(`${apiBaseUrl}/send-email`, {
