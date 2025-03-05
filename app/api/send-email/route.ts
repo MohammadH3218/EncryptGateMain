5b687c9145cf7afc4b32ae0761d95a1e6357ecb8
MohammadH3218 <hamdallahmohammad3219@gmail.com>
2025-03-05 15:59:34 -0600

    removed local env variables and put it on cloud for better security

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server';
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
 });
 
 const ses = new AWS.SES();
